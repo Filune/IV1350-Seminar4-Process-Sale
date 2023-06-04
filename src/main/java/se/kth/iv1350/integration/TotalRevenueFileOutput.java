780b94c4128716d3be1670b2aa4803d19080f82f
S. L <3577394+Filune@users.noreply.github.com>
2023-06-04 14:29:03 +0200

    Removed superfluous variable.

--- src/main/java/se/kth/iv1350/integration/TotalRevenueFileOutput.java
+++ src/main/java/se/kth/iv1350/integration/TotalRevenueFileOutput.java
@@ -18,21 +18,20 @@ public class TotalRevenueFileOutput implements PaymentObserver {
     private LogHandler logger = LogHandler.getInstance();
 
     /**
-     * Updates the total income with the provided newTotalToAdd value and writes it to a file.
+     * The total parameter is written to file.
      *
-     * @param newTotalToAdd The new total amount to add to the total income.
+     * @param totalToWriteToFile The new total amount to add to the total income.
      */
     @Override
-    public void updateTotalIncome(Total newTotalToAdd) {
+    public void updateTotalIncome(Total totalToWriteToFile) {
         String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         String content = String.format(
-            "[%s] Total Income: %s%n", timestamp, newTotalToAdd.getTotalPriceWithTax().getAmount());
+            "[%s] Total Income: %s%n", timestamp, totalToWriteToFile.getTotalPriceWithTax().getAmount());
 
         try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME, true))) {
             writer.println(content);
         } catch (IOException exception) {
             logger.logException(exception);
-            System.err.println("Failed to write total income to file: " + exception.getMessage());
         }
     }
 }
--- src/main/java/se/kth/iv1350/view/TotalRevenueView.java
+++ src/main/java/se/kth/iv1350/view/TotalRevenueView.java
@@ -8,26 +8,13 @@ import se.kth.iv1350.utility.Total;
  */
 public class TotalRevenueView implements PaymentObserver {
     
-    private Total totalIncome;
-
     /**
-     * Constructs a TotalRevenueView object.
-     * Initializes the totalIncome to a new Total object.
-     */
-    public TotalRevenueView(){
-        totalIncome = new Total();
-    }
-    
-    /**
-     * Updates the total income with the provided newTotalToAdd value.
-     * Calculates the updated total income by adding the newTotalToAdd to the current total income.
-     * Displays the updated total income on the console.
+     * Displays the total.
      * 
-     * @param newTotalToAdd The new total amount to add to the total income.
+     * @param totalToDisplay The new total amount to add to the total income.
      */
     @Override
-    public void updateTotalIncome(Total newTotalToAdd) {
-        this.totalIncome.updateTotal(newTotalToAdd);
-        System.out.println("DISPLAY FOR TotalRevenueView: " + totalIncome.getTotalPriceWithTax().getAmount());
+    public void updateTotalIncome(Total totalToDisplay) {
+        System.out.println("DISPLAY FOR TotalRevenueView: " + totalToDisplay.getTotalPriceWithTax().getAmount());
     }
 }
